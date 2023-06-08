65513b30a409038993eb8717726fadde9709c165
jusshin <jusshin@cisco.com>
2023-06-08 11:22:51 -0400

    adding

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print("This is a new change")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
